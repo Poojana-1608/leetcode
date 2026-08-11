81880aa1d485111f5bf7cd3209a0c0ad052d5d3e
Poojana-1608 <poojana1608@gmail.com>
2026-08-11 11:33:40 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,11 @@
+// Last updated: 8/11/2026, 11:33:36 AM
+class Solution {
+    public int[] countBits(int n) {
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            dp[i]=dp[i/2]+(i%2);
+        }
+        return dp;
+    }
+}
