2f9c1f45500bd25e1d99e4c149212b1897c4b253
Poojana-1608 <poojana1608@gmail.com>
2026-08-11 11:36:20 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,17 @@
+// Last updated: 8/11/2026, 11:36:17 AM
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+   
+        int i = 0, j = arr[0].length - 1;
+        while (i < arr.length && j >= 0) {
+            if (arr[i][j] == target)
+                return true;
+            else if (arr[i][j] < target)
+                i++;
+            else
+                j--;
+        }
+        return false;
+    }
+}
+    
