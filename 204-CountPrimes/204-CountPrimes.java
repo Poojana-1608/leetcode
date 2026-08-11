fbd18e2314588648cdfba7ca8275a2538e2c266a
Poojana-1608 <poojana1608@gmail.com>
2026-08-11 11:34:47 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.java
@@ -0,0 +1,44 @@
+// Last updated: 8/11/2026, 11:34:44 AM
+class Solution {
+
+    public int countPrimes(int n) {
+
+        if (n <= 2) {
+            return 0;
+        }
+
+        boolean[] prime = new boolean[n];
+
+        // Assume all are prime
+        for (int i = 0; i < n; i++) {
+            prime[i] = true;
+        }
+
+        prime[0] = false;
+        prime[1] = false;
+
+        // Sieve logic
+        for (int i = 2; i * i < n; i++) {
+
+            if (prime[i]) {
+
+                for (int j = i * i; j < n; j += i) {
+
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+
+            if (prime[i]) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
